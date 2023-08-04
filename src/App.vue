8289e085bb3c709f6b179a63856a455f91a8f3cc
Dotnet_Vastness <yh244396816@163.com>
2023-08-04 16:46:13 +0800

    add all vue3-piczoom

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import img from '@/assets/1.jpg'
+</script>
+
+<template>
+  <HelloWorld :url="img" :scale="3" :scroll="true" ></HelloWorld>
+</template>
+
