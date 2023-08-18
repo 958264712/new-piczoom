69f404cd428751c678fde644029c1962fb29bf78
wliu <lw958264712@163.com>
2023-08-18 09:39:33 +0800

    edit package and delete vite.config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import PicZoom from 'vue3-piczoom'
 
-createApp(App).use(PicZoom).mount('#app')
+
+createApp(App).mount('#app')
